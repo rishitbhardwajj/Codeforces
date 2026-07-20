ec709cf0885fd7026484b049fb6d01e0ebd607fb
Rishit Bhardwaj <bhardwajrishit2@gmail.com>
2026-07-20 22:14:34 +0530

    panormic

---
+++ A_Panoramix_s_Prediction.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = n + 1; i <= 50; i++) {
+        if (isPrime(i)) {
+            if (i == m)
+                cout << "YES";
+            else
+                cout << "NO";
+            break;
+        }
+    }
+
+    return 0;
+}
